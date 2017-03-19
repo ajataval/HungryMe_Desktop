0b0443669acf121cf7e5a111fab2dfaab0765331
kedar235 <kedar235@gmail.com>
2017-03-18 17:56:06 -0700

    FIXED API for App user login

--- routes/AccountManagementService.js
+++ routes/AccountManagementService.js
@@ -4,6 +4,11 @@
 var express = require('express');
 var router = express.Router();
 
+/*const monk = require('monk');
+const url = 'mongodb://serteam6:hungryme123@ds153179.mlab.com:53179/hungryme';
+const db = monk(url);*/
+
+
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
@@ -64,6 +69,54 @@ router.get('/app/users/:username', function (req, res, next) {
         });
     }
 });
+
+// login App User
+router.post('/app/login', function (req, res, next) {
+    username = req.body.username;
+    password = req.body.password;
+    if (username == undefined || username == ""|| password == undefined || password == "") {
+        err = new Error("Username/ Password cannot be empty");
+        err.status = 400;
+        next(err);
+    }
+    else {
+        //call mongodb
+        MongoClient.connect(url, function (err, db) {
+            db.collection('User').findOne({"username":username}).then(function (success) {
+                //console.log(success);
+
+                if(success == undefined) {
+                    //username not present
+                    success ={"result" : false};
+                    db.close();
+                    res.status(401);
+                    res.json(success)
+                }
+                else if( password !== success.password) {
+                    //password does not match
+                    success ={"result" : false};
+                    db.close();
+                    res.status(401);
+                    res.json(success)
+                }
+                else {
+                    //user authentication successful
+                    success ={"result" : true};
+                    db.close();
+                    res.status(200);
+                    res.json(success)
+                }
+
+            }, function (err) {
+                err = new Error("Server Error while searching for "+ username);
+                err.status=500;
+                db.close();
+                next(err);
+            });
+        });
+    }
+});
+
 
 //Add new Hotel User to MongoDB
 router.post('/hotel/users', function(req, res,next) {
