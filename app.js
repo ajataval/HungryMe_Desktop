532cb33f3affaec83e7588887a15a5046aa7b43e
kedar235 <kedar235@gmail.com>
2017-03-18 19:04:09 -0700

    Dummy API for search Request

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var acctMgmtSvc = require('./routes/AccountManagementService');
+var search = require('./routes/Search');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/', acctMgmtSvc);
+app.use('/', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/Search.js
@@ -0,0 +1,41 @@
+/**
+ * Created by KD on 3/2/2017.
+ */
+var express = require('express');
+var router = express.Router();
+
+/*const monk = require('monk');
+const url = 'mongodb://serteam6:hungryme123@ds153179.mlab.com:53179/hungryme';
+const db = monk(url);*/
+
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://serteam6:hungryme123@ds153179.mlab.com:53179/hungryme';
+
+//Get Hotel User by username from MongoDB
+router.get('/dummy', function (req, res, next) {
+    searchResult = [{
+        "hotelname" : "Hotel1",
+        "username": "testEmail1@email.com",
+        "distance":3.4,
+        "openNow":true,
+        "menu":[{"name":"item1","description":"Description1", "review": 3, "comment": ["cmt1","cmt2", "cmt3"]},
+            {"name":"item2","description":"description2","review":5,"comment": ["cmt1","cmt2", "cmt3"]}],
+        "address":"1255, E University Dr, Tempe, AZ 85281",
+        "cuisine":"Italian"
+    }, {
+        "hotelname" : "Hotel2",
+        "username": "testEmail2@email.com",
+        "distance":4.6,
+        "openNow":true,
+        "menu":[{"name":"dish1","description":"dish1", "review": 2,"comment": ["cmt1","cmt2", "cmt3"]},
+            {"name":"dish2","description":"dish2","review":4,"comment": ["cmt1","cmt2", "cmt3"]}],
+        "address":"1207, E 8 Street, Tempe, AZ 85281",
+        "cuisine":"American"
+    }];
+    res.json(searchResult)
+});
+
+module.exports = router;
