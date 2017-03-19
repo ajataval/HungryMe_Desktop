eba02fb2d189eb1276e676d5082ec436eb4b293d
kedar235 <kedar235@gmail.com>
2017-03-19 14:01:59 -0700

    Modified stubbed Json object

--- routes/Search.js
+++ routes/Search.js
@@ -4,15 +4,9 @@
 var express = require('express');
 var router = express.Router();
 
-/*const monk = require('monk');
+const monk = require('monk');
 const url = 'mongodb://serteam6:hungryme123@ds153179.mlab.com:53179/hungryme';
-const db = monk(url);*/
-
-
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://serteam6:hungryme123@ds153179.mlab.com:53179/hungryme';
+const db = monk(url);
 
 //Get Hotel User by username from MongoDB
 router.get('/dummy', function (req, res, next) {
@@ -20,7 +14,7 @@ router.get('/dummy', function (req, res, next) {
         "hotelname" : "Hotel1",
         "username": "testEmail1@email.com",
         "distance":3.4,
-        "openNow":true,
+        "openNow":"true",
         "menu":[{"name":"item1","description":"Description1", "review": 3, "comment": ["cmt1","cmt2", "cmt3"]},
             {"name":"item2","description":"description2","review":5,"comment": ["cmt1","cmt2", "cmt3"]}],
         "address":"1255, E University Dr, Tempe, AZ 85281",
@@ -29,7 +23,7 @@ router.get('/dummy', function (req, res, next) {
         "hotelname" : "Hotel2",
         "username": "testEmail2@email.com",
         "distance":4.6,
-        "openNow":true,
+        "openNow":"true",
         "menu":[{"name":"dish1","description":"dish1", "review": 2,"comment": ["cmt1","cmt2", "cmt3"]},
             {"name":"dish2","description":"dish2","review":4,"comment": ["cmt1","cmt2", "cmt3"]}],
         "address":"1207, E 8 Street, Tempe, AZ 85281",
@@ -37,5 +31,7 @@ router.get('/dummy', function (req, res, next) {
     }];
     res.json(searchResult)
 });
+
+
 
 module.exports = router;
