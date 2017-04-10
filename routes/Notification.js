b04c60190cfe21eddd4e534624d4345e9669f509
kedar235 <kedar235@gmail.com>
2017-04-09 19:02:29 -0700

    New Feature to mark hotels as favorite

--- routes/Notification.js
+++ routes/Notification.js
@@ -8,24 +8,22 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = process.env.MONGO_URL;
-var fcm_admin = require("firebase-admin");
+
 var FCM = require('fcm-push');
 var serverKey = process.env.FCM_SERVER_KEY;
-
 var fcm = new FCM(serverKey);
 
-router.post("/hotel/:username/happy_hour",
-    function (req,res,next){
+var admin = require("firebase-admin");
+var serviceAccount = require(__dirname+"/hungry-me-e36d3-firebase-adminsdk-90xb4-a59e713bf3.json");
+
+var inserthappyhour = function (req,res,next){
     req.body.username = req.params.username;
     req.body.published = "false";
     happyhour = req.body;
 
-
     MongoClient.connect(url, function (err, db) {
         db.collection('happyhour').insertOne(happyhour).then(function (success) {
             db.close();
-            //res.status(201);
-            //res.json({"result":true});
             next();
         }, function (err) {
             err = new Error("Could Not add happyhour to DB")
@@ -34,57 +32,10 @@ router.post("/hotel/:username/happy_hour",
             next(err);
         });
     });
+}
 
-}, function sendNotification(req,res,next){
-       /* /!*fcm_admin.initializeApp({
-            credential: fcm_admin.credential.cert({
-                projectId: process.env.GOOG_PRJ_ID,
-                clientEmail: process.env.GOOG_FCM_CLIENT,
-                privateKey: process.env.GOOG_FCM_PRV_KEY
-            }),
-            databaseURL: process.env.GOOG_FCM_DB_URL
-        });*!/
 
-        var serviceAccount = require(__dirname+"/hungry-me-e36d3-firebase-adminsdk-90xb4-55f746e39f.json");
-
-        /!*fcm_admin.initializeApp({
-            credential: fcm_admin.credential.cert(serviceAccount),
-            databaseURL: "https://hungry-me-e36d3.firebaseio.com"
-        });*!/
-
-        fcm_admin.initializeApp({
-            credential: fcm_admin.credential.refreshToken("AAAA5HSMyZ0:APA91bG7QQTpkBdbrlcmg2jyboI9muSY1xT4ExdlyC4I32-Vl3GQc0MKag2Z2dcyLgOyeyoESInqY2XVWuF6YxZAtfGphfIZhdZUz2yR22atMwkpcIUl2qgmngLDEVpGcNs60DXMcB32"),
-            databaseURL: "https://hungry-me-e36d3.firebaseio.com"
-        });
-
-        // The topic name can be optionally prefixed with "/topics/".
-        var topic = req.params.username;
-
-        // See the "Defining the message payload" section below for details
-        // on how to define a message payload.
-        var payload = {
-            data:{
-                hotelname: req.body.hotelname,
-                message: req.body.message,
-                start_time: req.body.start_time,
-                end_time: req.body.end_time,
-                username: req.params.username
-            }
-        };*/
-
-       /* // Send a message to devices subscribed to the provided topic.
-        fcm_admin.messaging().sendToTopic(topic, payload)
-            .then(function(response) {
-                // See the MessagingTopicResponse reference documentation for the
-                // contents of response.
-                console.log("Successfully sent message:", response);
-                res.status(201);
-                res.json({"result":true});
-            })
-            .catch(function(error) {
-                console.log("Error sending message:", error);
-            });*/
-
+router.post("/hotels/:username/happy_hour", inserthappyhour , function sendNotification(req,res,next){
         var message = {
             to: '/topics/'+req.params.username, // required fill with device token or topics
             data:{
@@ -114,4 +65,137 @@ router.post("/hotel/:username/happy_hour",
             })
 });
 
+
+var addFavoriteHotel = function (req,res,next){
+
+    if (req.body.hotel == undefined || req.body.hotel == ""
+        || req.body.registrationToken == undefined || req.body.registrationToken == "") {
+        err = new Error("Hotel/ Registration Token cannot be empty");
+        err.status = 400;
+        next(err);
+    }
+
+    var appUser = req.params.username;
+    var hotel = req.body.hotel;
+
+    MongoClient.connect(url, function (err, db) {
+        db.collection('User').update({
+                "username": appUser,
+            },
+            {
+                $addToSet: {favorite: hotel }
+            }).then(function (success) {
+            db.close();
+            next();
+        }, function (err) {
+            err = new Error("Could Not add happyhour to DB")
+            err.status=400;
+            db.close();
+            next(err);
+        });
+    });
+}
+
+var removeFavoriteHotel = function (req,res,next){
+
+    if (req.body.hotel == undefined || req.body.hotel == ""
+        || req.body.registrationToken == undefined || req.body.registrationToken == "") {
+        err = new Error("Hotel/ Registration Token cannot be empty");
+        err.status = 400;
+        next(err);
+    }
+
+    var appUser = req.params.username;
+    var hotel = req.body.hotel;
+
+    MongoClient.connect(url, function (err, db) {
+        db.collection('User').update(
+            {"username": appUser},
+            { $pull: { favorite: hotel } }
+        ).then(function (success) {
+            db.close();
+            next();
+        }, function (err) {
+            err = new Error("Could not remove  hotel from DB")
+            err.status=400;
+            db.close();
+            next(err);
+        });
+    });
+}
+
+var getUpdatedUser = function (req,res,next){
+    var username = req.params.username;
+    MongoClient.connect(url, function (err, db) {
+        db.collection('User').findOne({"username":username})
+            .then(function (success) {
+                if(success == undefined)
+                    success = {};
+                db.close();
+                req.appUser = success;
+                next();
+            }, function (err) {
+                err = new Error("Server Error while searching for "+ username);
+                err.status=500;
+                db.close();
+                next(err);
+            });
+    });
+}
+
+router.put("/app/users/:username/favorite", addFavoriteHotel , getUpdatedUser, function subscribeToFCM(req,res,next){
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://hungry-me-e36d3.firebaseio.com"
+    });
+
+    // This registration token comes from the client FCM SDKs.
+    var registrationToken = req.body.registrationToken;
+
+    // The topic name can be optionally prefixed with "/topics/".
+    var topic = "/topics/"+req.body.hotel;
+
+    // Subscribe the device corresponding to the registration token to the
+    // topic.
+    admin.messaging().subscribeToTopic(registrationToken, topic)
+        .then(function(response) {
+            // See the MessagingTopicManagementResponse reference documentation
+            // for the contents of response.
+            console.log("Successfully subscribed to topic:", response);
+            res.status(200);
+            res.json(req.appUser);
+        })
+        .catch(function(error) {
+            console.log("Error subscribing to topic:", error);
+        });
+});
+
+router.delete("/app/users/:username/favorite", removeFavoriteHotel , getUpdatedUser, function unsubscribeToFCM(req,res,next){
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://hungry-me-e36d3.firebaseio.com"
+    });
+
+    // This registration token comes from the client FCM SDKs.
+    var registrationToken = req.body.registrationToken;
+
+    // The topic name can be optionally prefixed with "/topics/".
+    var topic = "/topics/"+req.body.hotel;
+
+    // Subscribe the device corresponding to the registration token to the
+    // topic.
+    admin.messaging().unsubscribeFromTopic(registrationToken, topic)
+        .then(function(response) {
+            // See the MessagingTopicManagementResponse reference documentation
+            // for the contents of response.
+            console.log("Successfully unsubscribed from topic:", response);
+            res.status(200);
+            res.json(req.appUser);
+        })
+        .catch(function(error) {
+            console.log("Error subscribing to topic:", error);
+        });
+});
 module.exports = router;
